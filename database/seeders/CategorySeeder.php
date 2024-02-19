4a2e0c6dc368dd3f749cea74fb4b75ff35137efd
Andy <amcdonald1307@protonmail.com>
2024-02-19 01:21:10 +0000

    seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,7 +14,7 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categorys')->insert([
-            'title' => 'homepage'
+            'title' => 'Homepage'
         ]);
         DB::table('categorys')->insert([
             'title' => 'about'
