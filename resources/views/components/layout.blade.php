c24e6bcdfcfd3a68652bf3c3e8b9cfd1e5ad9a55
Andy <amcdonald1307@protonmail.com>
2024-03-06 15:45:38 +0000

    Removed overflow y scroll

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,12 +8,13 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     @vite('resources/css/app.css')
 </head>
-<body class="font-figtree selection:bg-dark-600">
+<body class="font-figtree selection:bg-dark-600 overflow-y-scroll">
     <div class="flex flex-col overflow-hidden">
         <x-header />
         <main class="h-screen flex-grow bg-dark-100 text-white">
             {{ $slot }}
         </main>
+        <x-footer />
     </div>
 </body>
 </html>
